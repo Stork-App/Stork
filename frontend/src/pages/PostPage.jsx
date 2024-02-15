a3e712ddd66148cca28442130158df755ffd64b1
thisischu <chuyilei1@gmail.com>
2024-02-15 13:09:49 -0500

    individual post page

---
+++ frontend/src/pages/PostPage.jsx
@@ -0,0 +1,32 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getPost } from "../adapters/post-adapter";
+
+export default function PostPage() {
+    const [forumPost, setForumPost] = useState(null);
+    const [errorText, setErrorText] = useState(null);
+    const { id } = useParams();
+    
+  useEffect(() => {
+    const loadPost = async () => {
+      const [post, error] = await getPost(id);
+      if (error) return setErrorText(error.message);
+      setForumPost(post);
+    };
+
+    loadPost();
+  }, [id]);
+
+  if (!forumPost && !errorText) return null;
+  if (errorText) return <p>{errorText}</p>;
+
+  console.log(forumPost)
+
+    return <>
+
+       <h1>{forumPost.title}</h1>
+       <h1>{forumPost.description}</h1>
+
+       
+    </>
+}

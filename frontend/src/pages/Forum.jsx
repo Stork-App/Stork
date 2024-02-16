045540f702fcc7503176c484d2a8ead2560adcb2
thisischu <chuyilei1@gmail.com>
2024-02-16 14:39:34 -0500

    Forum create post completed

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -1,7 +1,9 @@
 import DisplayPosts from "../components/ForumComponents/DisplayPosts";
+import PostForm from "../components/ForumComponents/PostForm";
 
 export default function ForumPage() {
     return <>
     <DisplayPosts/>
+    <PostForm/>
     </>
 }

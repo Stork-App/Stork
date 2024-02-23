2dc39da08150164be7c877f32c12c353ba0bd9a8
thisischu <chuyilei1@gmail.com>
2024-02-23 14:15:28 -0500

    home page animations updated

--- frontend/src/components/HomeComponents/HomeArticles.jsx
+++ frontend/src/components/HomeComponents/HomeArticles.jsx
@@ -1,39 +1,73 @@
+import React, { useEffect } from 'react';
 import advocacyImg from '../images/AdvocacyImg.png';
-import chestPainImg from '../images/ChestPainImg.png'
+import chestPainImg from '../images/ChestPainImg.png';
 import disparitiesImg from '../images/DisparitiesImg.png';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 export default function HomeArticles() {
+  const controlsAdvocacy = useAnimation();
+  const controlsChestPain = useAnimation();
+  const controlsDisparities = useAnimation();
+
+  const [refAdvocacy, inViewAdvocacy] = useInView();
+  const [refChestPain, inViewChestPain] = useInView();
+  const [refDisparities, inViewDisparities] = useInView();
+
+  const articleAnimation = {
+    hidden: { y: 20, opacity: 0 },
+    visible: { y: 0, opacity: 1, transition: { duration: 1 } },
+  };
+
+  useEffect(() => {
+    if (inViewAdvocacy) {
+      controlsAdvocacy.start('visible');
+    } else {
+      controlsAdvocacy.start('hidden');
+    }
+  }, [controlsAdvocacy, inViewAdvocacy]);
+
+  useEffect(() => {
+    if (inViewChestPain) {
+      controlsChestPain.start('visible');
+    } else {
+      controlsChestPain.start('hidden');
+    }
+  }, [controlsChestPain, inViewChestPain]);
+
+  useEffect(() => {
+    if (inViewDisparities) {
+      controlsDisparities.start('visible');
+    } else {
+      controlsDisparities.start('hidden');
+    }
+  }, [controlsDisparities, inViewDisparities]);
+
   return (
     <>
       <section id="research-articles-section">
-        <h3 class="articles-heading">Relevant Studies</h3>
-        <section class="article-list">
-          <section class="article-item">
-            <img class="article-image-label" src={advocacyImg}/>
-            <p><b>
-              “A Pilot Exploring Self-Advocacy as a Factor in Maternal Mortality and Morbidity in Pregnant Black Women.”
-              </b></p>
-            <p class="article-metadata">D-Psychology at University of Pittsburgh, 1 Jan. 1970</p>
+        <h3 className="articles-heading">Relevant Studies</h3>
+        <section className="article-list">
+          <motion.section className="article-item" ref={refAdvocacy} initial="hidden" animate={controlsAdvocacy} variants={articleAnimation}>
+            <motion.img className="article-image-label" src={advocacyImg} variants={articleAnimation} />
+            <p><b>“A Pilot Exploring Self-Advocacy as a Factor in Maternal Mortality and Morbidity in Pregnant Black Women.”</b></p>
+            <p className="article-metadata">D-Psychology at the University of Pittsburgh, 1 Jan. 1970</p>
             <a href="https://d-scholarship.pitt.edu/40428/" target="_blank" rel="noopener noreferrer" className="learn-more-btn">Read More</a>
-          </section>
+          </motion.section>
 
-          <section class="article-item">
-          <img class="article-image-label" src={chestPainImg}/>
-            <p><b>
-              “Is self-advocacy universally achievable for patients? the experiences of Australian women with cardiac disease in pregnancy and postpartum.”
-            </b></p>
-            <p class="article-metadata">International Journal of Qualitative Studies on Health and Well-Being, vol. 18, no. 1, 23 Feb. 2023</p>
+          <motion.section className="article-item" ref={refChestPain} initial="hidden" animate={controlsChestPain} variants={articleAnimation}>
+            <motion.img className="article-image-label" src={chestPainImg} variants={articleAnimation} />
+            <p><b>“Is self-advocacy universally achievable for patients? the experiences of Australian women with cardiac disease in pregnancy and postpartum.”</b></p>
+            <p className="article-metadata">International Journal of Qualitative Studies on Health and Well-Being, vol. 18, no. 1, 23 Feb. 2023</p>
             <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9970247/" target="_blank" rel="noopener noreferrer" className="learn-more-btn">Read More</a>
-          </section>
+          </motion.section>
 
-          <section class="article-item">
-          <img class="article-image-label" src={disparitiesImg}/>
-            <p><b>
-              “Reducing Disparities in Severe Maternal Morbidity and Mortality.”
-              </b></p>
-            <p class="article-metadata">Clinical Obstetrics and Gynecology, U.S. National Library of Medicine, June 2018</p>
+          <motion.section className="article-item" ref={refDisparities} initial="hidden" animate={controlsDisparities} variants={articleAnimation}>
+            <motion.img className="article-image-label" src={disparitiesImg} variants={articleAnimation} />
+            <p><b>“Reducing Disparities in Severe Maternal Morbidity and Mortality.”</b></p>
+            <p className="article-metadata">Clinical Obstetrics and Gynecology, U.S. National Library of Medicine, June 2018</p>
             <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5915910/" target="_blank" rel="noopener noreferrer" className="learn-more-btn">Read More</a>
-          </section>
+          </motion.section>
         </section>
       </section>
     </>
--- frontend/src/components/HomeComponents/HomeHeader.jsx
+++ frontend/src/components/HomeComponents/HomeHeader.jsx
@@ -1,26 +1,68 @@
-import phoneCallIcon from '../images/PhoneCallIcon.png'
-import consultationIcon from '../images/ConsultationIcon.png'
-import ultrasoundIcon from '../images/UltrasoundIcon.png'
-import webPageIcon from '../images/WebpageIcon.png'
+import React, { useEffect } from 'react';
+import phoneCallIcon from '../images/PhoneCallIcon.png';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 export default function HomeHeader() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  const textAnimation = {
+    hidden: { y: -20, opacity: 0 },
+    visible: { y: 0, opacity: 1, transition: { duration: 1 } },
+  };
+
+  const imageAnimation = {
+    hidden: { y: 0 },
+    visible: { y: -20, transition: { duration: 1 } },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    } else {
+      controls.start('hidden');
+    }
+  }, [controls, inView]);
+
   return (
     <>
-      <section id="home-section1" className="home-section1-left">
-        <section className="home-content">
-          <h1 class="fade-in" className="main-heading" style={{fontSize: 80}}>
+      <section id="home-section1" className="home-section1-left" ref={ref}>
+        <motion.section
+          className="home-content"
+          initial="hidden"
+          animate={controls}
+          variants={textAnimation}
+        >
+          <motion.h1
+            className="main-heading"
+            style={{ fontSize: 80 }}
+            variants={textAnimation}
+          >
             Stork
-          </h1>
-          <p className="sub-text">
-            A comprehensive self advocacy tool created to bridge racial gaps in maternal childbirth mortality rates.
-          </p>
-          <a href="https://projects.apnews.com/features/2023/from-birth-to-death/black-women-maternal-mortality-rate.html#:~:text=Black%20women%20have%20the%20highest,for%20Disease%20Control%20and%20Prevention." target="_blank" rel="noopener noreferrer" className="learn-more-btn">Learn More</a>
-        </section>
+          </motion.h1>
+          <motion.p className="sub-text" variants={textAnimation}>
+            A comprehensive self-advocacy tool created to bridge racial gaps in maternal childbirth mortality rates.
+          </motion.p>
+          <a
+            href="https://projects.apnews.com/features/2023/from-birth-to-death/black-women-maternal-mortality-rate.html#:~:text=Black%20women%20have%20the%20highest,for%20Disease%20Control%20and%20Prevention."
+            target="_blank"
+            rel="noopener noreferrer"
+            className="learn-more-btn"
+          >
+            Learn More
+          </a>
+        </motion.section>
 
-        <section class="fade-in" id="home-header-image">
+        <motion.section
+          id="home-header-image"
+          className="fade-in"
+          initial="hidden"
+          animate={controls}
+          variants={imageAnimation}
+        >
           <img src={phoneCallIcon} alt="" />
-
-        </section>
+        </motion.section>
       </section>
     </>
   );
